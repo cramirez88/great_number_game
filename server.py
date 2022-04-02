50a496443370b808dc3e19bcfd027d882fe46f31
Christian Ramirez <christian.r0602@gmail.com>
2022-04-02 15:44:53 -0500

    adding routes

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect
+import random
 
 app = Flask(__name__)
+app.secret_key = 'lalalala'
 
 @app.route('/')
 def index():
+    if 'num' not in session:
+        session['num'] = random.randint(1,100)
+    print(session['num'])
     return render_template('index.html')
+
+@app.route('/guess', methods=['POST'])
+def guess():
+    session['guess'] = int(request.form['numgame'])
+    print(session['guess'])
+    return redirect('/')
+
+
+
 
 if __name__ == ('__main__'):
     app.run(debug=True)
